7dac6022403bb284d4f3ef6e6855ae3b98ada728
McDevon <jussi.enroos@gmail.com>
2019-09-07 10:35:35 +0300

    Update button styles and lose accessibility focus for now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Button = ({ onClick, text, disabled }) => {
-    const style = {
-        backgroundColor: disabled ? '#6CAF70' : '#4CAF50',
-        border: 'none',
-        borderRadius: '4px',
-        color: 'white',
-        padding: '10px 20px',
-        textAlign: 'center',
-        textDecoration: 'none',
-        display: 'inline-block',
-        fontSize: '16px',
-        opacity: disabled ? '0.6' : '1',
-        "&:hover": {
-            backgroundColor: "#efefef"
-          }
+
+    const StyledButton = styled.button`
+    background-color: ${disabled ? '#c2c9b7' : '#4CAF50'};
+    border: none;
+    border-radius: 4px;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 0px;
+    font-size: 16px;
+    opacity: ${disabled ? '0.6' : '1'};
+    :hover {
+        background-color: ${disabled ? '#c2c9b7' : '#5bc146'};
     }
-return  <button style={style} onClick={onClick} disabled={disabled}>
-      {text}
-    </button>
-  }
+    :focus {
+        outline: none;
+    }
+    :active {
+        padding: 9px 18px;
+        font-size: 14px;
+        margin: 1px 4px;
+    }
+    `
+
+    const divStyle = {
+        height: '42px'
+    }
+
+    return <div style={divStyle}>
+        <StyledButton onClick={onClick} disabled={disabled}>
+            {text}
+        </StyledButton>
+    </div>
+}
 
 export default Button
